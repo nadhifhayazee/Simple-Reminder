3df221ddff47365f8aa71e129128bac22bbaf83a
nadhifhayazee <nadhifhayazee@gmail.com>
2026-04-23 16:05:11 +0700

    (adjust): increment versionCode for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.nadhifhayazee.simplereminder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
