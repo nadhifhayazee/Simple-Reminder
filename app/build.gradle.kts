303133410f2ce675789f58b9ab5fa2820a9609fb
nadhifhayazee <nadhifhayazee@gmail.com>
2026-04-28 12:02:38 +0700

    chore: change track to production and increment versionCode to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.nadhifhayazee.simplereminder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
