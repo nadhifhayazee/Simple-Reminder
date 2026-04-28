c9db7d2999121302c4c001d2717821b309b99b98
nadhifhayazee <nadhifhayazee@gmail.com>
2026-04-28 11:31:40 +0700

    chore: increment versionCode to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.nadhifhayazee.simplereminder"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
